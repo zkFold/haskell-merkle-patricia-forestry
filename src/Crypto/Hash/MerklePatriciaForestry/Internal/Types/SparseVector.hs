020a2424f92619325d7f8468c25933b8fcd775c8
sourabhxyz <lambdaproofs@pm.me>
2025-06-19 22:39:29 +0530

    feat: get rid of `vector` dependency

--- src/Crypto/Hash/MerklePatriciaForestry/Internal/Types/SparseVector.hs
+++ src/Crypto/Hash/MerklePatriciaForestry/Internal/Types/SparseVector.hs
@@ -7,11 +7,10 @@ module Crypto.Hash.MerklePatriciaForestry.Internal.Types.SparseVector (
 import Crypto.Hash.MerklePatriciaForestry.Internal.Types.HexDigit (HexDigit, allHexDigits)
 import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as Map
-import Data.Vector qualified as V
 
 -- | Vector of length 16. See 'mkSparseVector' for details relating construction.
-newtype SparseVector a = SparseVector {unSparseVector :: V.Vector (Maybe a)}
+newtype SparseVector a = SparseVector {unSparseVector :: [Maybe a]}
 
 -- | Construct a sparse vector from a map. We put 'Nothing' for indices that are not present in the map.
 mkSparseVector :: forall a. Map HexDigit a -> SparseVector a
-mkSparseVector m = SparseVector $ V.fromList $ map (`Map.lookup` m) allHexDigits
+mkSparseVector m = SparseVector $ map (`Map.lookup` m) allHexDigits
